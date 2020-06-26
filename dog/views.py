c5655d3fbc5744cb2717551f90ad2bc78a3b2e7e
Chanyoon Zhu <zhqianyu@amazon.com>
2020-06-26 10:50:45 -0700

    Makes /api/v1/dogs/1 work

--- dog/views.py
+++ dog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Dog
 from .serializers import DogSerializer
 
 # Create your views here.
 
 class DogList(ListAPIView):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+class DogDetail(RetrieveAPIView):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
 
