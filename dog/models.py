1ccdaef96e81f4861a0931900d6594040b139a77
Chanyoon Zhu <zhqianyu@amazon.com>
2020-06-26 11:33:06 -0700

    Adds a foreign key

--- dog/models.py
+++ dog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=64)
     breed = models.CharField(max_length=128)
     age = models.IntegerField()
+    favorite_human = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
 
 def __str__(self):
     return f'{self.name}:{self.breed}:{self.age}'
